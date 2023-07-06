e4ce257a429a42da1a3879455bb7323bbac440cb
Nathan Bradshaw <nathansbradshaw@gmail.com>
2023-07-05 21:06:13 -0500

    remove test containers

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -8,30 +8,3 @@ pub fn routes() -> Router {
 pub async fn health_check() -> impl IntoResponse {
     StatusCode::OK
 }
-
-#[cfg(test)]
-mod test {
-    use crate::app::test_util_app;
-    use axum::{
-        body::Body,
-        http::{self, Request, StatusCode},
-    };
-    use tower::ServiceExt;
-
-    #[tokio::test]
-    async fn should_retun_200_ok() {
-        let (app, _address, _listener, _) = test_util_app::setup().await;
-        let response = app
-            .oneshot(
-                Request::builder()
-                    .method(http::Method::GET)
-                    .uri("/api/health")
-                    .body(Body::empty())
-                    .unwrap(),
-            )
-            .await
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-    }
-}
